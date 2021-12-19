2b102e7454e5be157d11d8b2c32e2e21d66554d4
nahee-park <jessica396@ewhain.net>
2021-12-19 22:23:04 +0900

    refact: 반복되는 구문 함수로 리팩토링

--- Javascript/Carousel/index.js
+++ Javascript/Carousel/index.js
@@ -1,30 +1,33 @@
 const item = document.querySelector('.item');
 const prevBtn = document.querySelector('.btn__prev');
 const nextBtn = document.querySelector('.btn__next');
-const IMG_WIDTH = 500;
 
-const imageArray = ["https://file.mk.co.kr/meet/neds/2021/06/image_readtop_2021_535745_16226846584668330.jpg", "https://www.newspenguin.com/news/photo/202101/3899_12249_529.jpg","http://cdn.ksilbo.co.kr/news/photo/202106/902252_501806_3356.png"]
+const imageArray = ["https://file.mk.co.kr/meet/neds/2021/06/image_readtop_2021_535745_16226846584668330.jpg", "https://www.newspenguin.com/news/photo/202101/3899_12249_529.jpg","http://cdn.ksilbo.co.kr/news/photo/202106/902252_501806_3356.png","https://t1.daumcdn.net/cfile/tistory/216C553953FC27C335"]
+
+const IMG_WIDTH = 500;
+const LAST_INDEX = imageArray.length-1;
+const FIRST_INDEX = 0;
 let currentItem = 0;
 let position = -IMG_WIDTH;
 
-const createAdditionalItem = (idx, classId) =>{
-    const plusImg = document.createElement('img');
-    plusImg.setAttribute('src', imageArray[idx]);
-    plusImg.setAttribute('class', `item${classId}`);
-    item.appendChild(plusImg);
+const createFakeItem = (idx, classId) =>{
+    const fakeImg = document.createElement('img');
+    fakeImg.setAttribute('src', imageArray[idx]);
+    fakeImg.setAttribute('class', `item${classId}`);
+    item.appendChild(fakeImg);
 }
 
 const createCarouselItem = () =>{
     imageArray.map((o,key)=>{
-        if (key === 0) {
-            createAdditionalItem(imageArray.length-1, 0);
+        if (key === FIRST_INDEX) {
+            createFakeItem(LAST_INDEX, 0);
         }
         const img = document.createElement('img');
         img.setAttribute('src', o);
         img.setAttribute('class', `item${key+1}`);
         item.appendChild(img);
-        if (key === imageArray.length-1) {
-            createAdditionalItem(0, imageArray.length+1);
+        if (key === LAST_INDEX) {
+            createFakeItem(FIRST_INDEX, imageArray.length+1);
         }
     })
 }
@@ -32,45 +35,45 @@ const createCarouselItem = () =>{
 const slideItem = async (type) => {
     let newPosition;
     if (type==="prev") {
-        item.animate([{transform:`translateX(${position}px)`}, {transform:`translateX(${position+IMG_WIDTH}px)`}],300);
         newPosition = position+IMG_WIDTH;
+        item.animate([{transform:`translateX(${position}px)`}, {transform:`translateX(${newPosition}px)`}],300);
     } else {
-        item.animate([{transform:`translateX(${position}px)`}, {transform:`translateX(${position-IMG_WIDTH}px)`}],300);
         newPosition = position-IMG_WIDTH;
+        item.animate([{transform:`translateX(${position}px)`}, {transform:`translateX(${newPosition}px)`}],300);
     }
     return newPosition;
 }
 
+const transformItem = () => {
+    item.style.transform = `translateX(${position}px)`;
+}
+
 const prev = async () => {
     position = await slideItem("prev");
-    if(currentItem === 0) {
-        currentItem = imageArray.length-1;
+    if(currentItem === FIRST_INDEX) {
+        currentItem = LAST_INDEX;
         position= -IMG_WIDTH*imageArray.length;
     } else{
         currentItem -= 1;
     }
-    item.style.transform = `translateX(${position}px)`;
+    transformItem();
 }
 const next = async () => {
     position = await slideItem("next");
-    if(currentItem === imageArray.length-1) {
-        currentItem = 0;
+    if(currentItem === LAST_INDEX) {
+        currentItem = FIRST_INDEX;
         position= -IMG_WIDTH;
     } else{
         currentItem += 1;
     }
-    item.style.transform = `translateX(${position}px)`;
+    transformItem();
 }
 
 const main = () => {
     createCarouselItem();
-    item.style.transform = `translateX(${position}px)`;
+    transformItem();
     prevBtn.addEventListener("click",prev);
     nextBtn.addEventListener("click",next);
 }
 
 main();
-// 맨 처음엔 첫번째 아이템만 show클래스 추가, 
-// 일단 imageArray 돌면서 Img테그 생성, 클래스 이름 전부 다르게 
-
-// currentItem 변수에서 next 클릭하면 다음으로 넘어가고 prev 클릭하면 
