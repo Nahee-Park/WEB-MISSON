eda6daca25a9a5491e0323f0d83a97fc5b966ca7
nahee-park <jessica396@ewhain.net>
2022-01-28 22:38:31 +0900

    feat: 코드리뷰 반영

--- Javascript/Carousel/index.js
+++ Javascript/Carousel/index.js
@@ -1,55 +1,49 @@
-const item = document.querySelector('.item');
-const prevBtn = document.querySelector('.btn__prev');
-const nextBtn = document.querySelector('.btn__next');
+const itemEl = document.querySelector('.item');
+const prevBtnEl = document.querySelector('.btn__prev');
+const nextBtnEl = document.querySelector('.btn__next');
 
 const imageArray = ["https://file.mk.co.kr/meet/neds/2021/06/image_readtop_2021_535745_16226846584668330.jpg", "https://www.newspenguin.com/news/photo/202101/3899_12249_529.jpg","http://cdn.ksilbo.co.kr/news/photo/202106/902252_501806_3356.png","https://t1.daumcdn.net/cfile/tistory/216C553953FC27C335"]
 
 const IMG_WIDTH = 500;
 const LAST_INDEX = imageArray.length-1;
 const FIRST_INDEX = 0;
+const DIRECTION_LEFT = 'prev';
+const DIRECTION_RIGHT = 'next';
 let currentItem = 0;
 let position = -IMG_WIDTH;
 
-const createFakeItem = (idx, classId) =>{
-    const fakeImg = document.createElement('img');
-    fakeImg.setAttribute('src', imageArray[idx]);
-    fakeImg.setAttribute('class', `item${classId}`);
-    item.appendChild(fakeImg);
+const createItem = (idx, slideItemArr) =>{
+    const img = document.createElement('img');
+    img.setAttribute('src', slideItemArr[idx]);
+    img.setAttribute('class', `item${idx}`);
+    return img;
+}
+
+const appendItem = (item) => {
+    itemEl.appendChild(item);
 }
 
 const createCarouselItem = () =>{
-    imageArray.map((o,key)=>{
-        if (key === FIRST_INDEX) {
-            createFakeItem(LAST_INDEX, 0);
-        }
-        const img = document.createElement('img');
-        img.setAttribute('src', o);
-        img.setAttribute('class', `item${key+1}`);
-        item.appendChild(img);
-        if (key === LAST_INDEX) {
-            createFakeItem(FIRST_INDEX, imageArray.length+1);
-        }
+    const slideItemArr = [...imageArray]
+    slideItemArr.unshift(imageArray[LAST_INDEX]);
+    slideItemArr.push(imageArray[FIRST_INDEX]);
+    slideItemArr.map((src,idx)=>{
+        appendItem(createItem(idx, slideItemArr));
     })
 }
 
 const slideItem =  (type) => {
-    let newPosition;
-    if (type==="prev") {
-        newPosition = position+IMG_WIDTH;
-        item.animate([{transform:`translateX(${position}px)`}, {transform:`translateX(${newPosition}px)`}],300);
-    } else {
-        newPosition = position-IMG_WIDTH;
-        item.animate([{transform:`translateX(${position}px)`}, {transform:`translateX(${newPosition}px)`}],300);
-    }
-    return newPosition;
+    const distanceToMove = type === DIRECTION_LEFT ? position + IMG_WIDTH : position - IMG_WIDTH;
+    itemEl.animate([{transform:`translateX(${position}px)`}, {transform:`translateX(${distanceToMove}px)`}],300);
+    return distanceToMove;
 }
 
 const transformItem = () => {
-    item.style.transform = `translateX(${position}px)`;
+    itemEl.style.transform = `translateX(${position}px)`;
 }
 
 const prev =  () => {
-    position =  slideItem("prev");
+    position =  slideItem(DIRECTION_LEFT);
     if(currentItem === FIRST_INDEX) {
         currentItem = LAST_INDEX;
         position= -IMG_WIDTH*imageArray.length;
@@ -59,7 +53,7 @@ const prev =  () => {
     transformItem();
 }
 const next =  () => {
-    position =  slideItem("next");
+    position =  slideItem(DIRECTION_RIGHT);
     if(currentItem === LAST_INDEX) {
         currentItem = FIRST_INDEX;
         position= -IMG_WIDTH;
@@ -72,8 +66,8 @@ const next =  () => {
 const main = () => {
     createCarouselItem();
     transformItem();
-    prevBtn.addEventListener("click",prev);
-    nextBtn.addEventListener("click",next);
+    prevBtnEl.addEventListener("click",prev);
+    nextBtnEl.addEventListener("click",next);
 }
 
 main();
